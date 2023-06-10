6501efcfbc15b37d776c7e19ab5ef5beb5eb7813
daniel sasi <daniel.sasi.74@gmail.com>
2023-06-10 17:56:57 +0300

    BMI_Calculator2.2.1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,11 @@
 public class Calculator {
-    private double BMI;
-    private double idealWeight;
-    private String WeightStatus;
-
 
     public double calculateBMI(double height , double actualWeight){
-        this.BMI = actualWeight / (height * height);
-
-        return this.BMI;
+        return actualWeight / (height * height);
     }
 
     public double calculateIdealWeight(double bodyFrame , double height , double age){
-        this.idealWeight = (height - 100 + (age / 10)) * 0.9 * bodyFrame;
+        double idealWeight = (height - 100 + (age / 10)) * 0.9 * bodyFrame;
         System.out.println(height);
         System.out.println(age);
         System.out.println(bodyFrame);
@@ -19,26 +13,27 @@ public class Calculator {
     }
 
     public String calculateWeightStatus(double BMI){
+        String weightStatus;
         if (BMI < 15){
-            return this.WeightStatus = "Anorexic";
+            return weightStatus = "Anorexic";
         }
         if (BMI >= 15 && BMI < 18.5){
-            return this.WeightStatus = "Underweight";
+            return weightStatus = "Underweight";
         }
         if (BMI >= 18.5 && BMI < 24.9){
-            return this.WeightStatus = "Normal";
+            return weightStatus = "Normal";
         }
         if (BMI >= 24.9 && BMI < 29.9){
-            return this.WeightStatus = "Overweight";
+            return weightStatus = "Overweight";
         }
         if (BMI >= 29.9 && BMI < 35){
-            return this.WeightStatus = "Obese";
+            return weightStatus = "Obese";
         }
         if (BMI >= 35){
-            return this.WeightStatus = "Extreme Obese";
+            return weightStatus = "Extreme Obese";
         }
-        this.WeightStatus = "invalid";
-        return this.WeightStatus;
+        weightStatus = "invalid";
+        return weightStatus;
     }
 
 
